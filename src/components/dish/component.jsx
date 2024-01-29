e5207ac6731c796f050e0e669570f92e582be392
Andrei Dubinko <andreidubinko@Andreis-MacBook-Pro-2.local>
2024-01-29 14:13:37 +0200

    hw3 1 of 2 part

--- src/components/dish/component.jsx
+++ src/components/dish/component.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
+
 export const Dish = ({dish}) => {
+
+  const [count, setCount] = useState(0);
+
+  const counterInc = () => {
+    if(count < 5){
+      setCount(count + 1);
+    }
+  }
+
+  const countDec = () => {
+    if(count > 0){
+      setCount(count - 1);
+    }
+  }
+
+  const disableInc = count === 5;
+  const disableDec = count === 0;
+
   return (
   <div>
     {dish.name}
+    <span className="counter">
+      <button disabled={disableDec} onClick={countDec}>-</button>
+      {count}
+      <button disabled={disableInc} onClick={counterInc}>+</button>
+    </span>
   </div>
   );
 }
