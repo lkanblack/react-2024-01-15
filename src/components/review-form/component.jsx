27af41a2375b69133440e1211032ad5776e3e730
Andrei Dubinko <andrei.dubinko@krabutech.ee>
2024-01-31 14:33:53 +0200

    lecture-4/hw form components (useReducer)

---
+++ src/components/review-form/component.jsx
@@ -0,0 +1,67 @@
+import { useReducer } from "react";
+import styles from "./style.module.scss";
+
+const initialState = {
+  name: "",
+  text: "",
+  rating: 1,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_NAME":
+      return { ...state, name: action.payload };
+    case "SET_TEXT":
+      return { ...state, text: action.payload };
+    case "SET_RATING":
+      return { ...state, rating: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const ReviewForm = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Name:", state.name);
+    console.log("Text:", state.text);
+    console.log("Rating:", state.rating);
+  };
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        value={state.name}
+        onChange={(e) => dispatch({ type: "SET_NAME", payload: e.target.value })}
+      />
+
+      <label htmlFor="text">Text</label>
+      <textarea
+        type="text"
+        name="text"
+        id="text"
+        value={state.text}
+        onChange={(e) => dispatch({ type: "SET_TEXT", payload: e.target.value })}
+      />
+
+      <label htmlFor="rating">Rating</label>
+      <input
+        type="number"
+        min={1}
+        max={5}
+        name="rating"
+        id="rating"
+        value={state.rating}
+        onChange={(e) => dispatch({ type: "SET_RATING", payload: e.target.value })}
+      />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
