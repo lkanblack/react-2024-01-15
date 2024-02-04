14540d334d3309c63a1d41bce5c77aacfaed0121
Andrei Dubinko <andreidubinko@Andreis-MacBook-Pro-2.local>
2024-02-04 16:24:03 +0200

    login button

--- src/components/header/component.jsx
+++ src/components/header/component.jsx
@@ -1,7 +1,11 @@
 import styles from './styles.module.scss';
+import { LoginButton } from '../login-button/component';
 
 export const Header = () => {
   return(
-    <header className={styles.root}>Header</header>
+    <header className={styles.root}>
+      Header
+      <LoginButton />
+      </header>
   )
 }
---
+++ src/components/login-button/component.jsx
@@ -0,0 +1,7 @@
+import style from './style.module.scss';
+
+export const LoginButton = () => {
+  return (
+    <button className={style.login}>Login</button>
+  )
+}
