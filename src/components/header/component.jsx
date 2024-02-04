31e633e772c705754b0011e5fbac4454a43c68ff
Andrei Dubinko <andreidubinko@Andreis-MacBook-Pro-2.local>
2024-02-04 18:27:33 +0200

    login changes name of context in header

--- src/components/header/component.jsx
+++ src/components/header/component.jsx
@@ -1,11 +1,17 @@
 import styles from './styles.module.scss';
 import { LoginButton } from '../login-button/component';
+import { UserContext } from '../../contexts/user';
+import { useState } from 'react';
 
 export const Header = () => {
+
+  const [context, setContext] = useState("Guest");
   return(
     <header className={styles.root}>
-      Header
-      <LoginButton />
+      <UserContext.Provider value={[context, setContext]}>
+        <h2>Welcome, {context.user ? context.user : context}</h2>
+        <LoginButton />
+      </UserContext.Provider>
       </header>
   )
 }
--- src/components/login-button/component.jsx
+++ src/components/login-button/component.jsx
@@ -1,7 +1,17 @@
 import style from './style.module.scss';
+import { UserContext } from '../../contexts/user';
+import { useContext, useState } from 'react';
 
 export const LoginButton = () => {
+  const [isLogged, setIsLogged] = useState(false);
+  const [context, setContext] = useContext(UserContext);
+
+  const handleContext = () => {
+    setContext({user:"John Wick", email:"john@wick.com"});
+    setIsLogged(true);
+  }
+
   return (
-    <button className={style.login}>Login</button>
+    <button className={style.login} onClick={handleContext}>{isLogged ? 'Logout':'Login'}</button>
   )
 }
