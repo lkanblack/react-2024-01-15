c36902e1580e924fbe80829ada7d189027d90774
Andrei Dubinko <andrei.dubinko@krabutech.ee>
2024-02-08 10:58:51 +0200

    review components filled with author name

--- src/components/review/comment.jsx
+++ src/components/review/comment.jsx
@@ -1,14 +1,26 @@
 import { useSelector } from "react-redux";
 import { selectReviewById } from "../../redux/entities/reviews/selector";
+import { selectUserById } from "../../redux/entities/users/selector";
 
-export const Review = ({id}) => {
+export const Review = ({ id }) => {
+  const review = useSelector((state) => selectReviewById(state, id));
+  const userId = review?.userId;
+  const user = useSelector((state) => userId ? selectUserById(state, userId) : null);
 
-  const review = useSelector((state) =>selectReviewById(state, id))
-  return(
+  return (
     <>
       {review ? (
-        <div>{review.text}</div>
-      ) : ( <div>Review not found</div> )}
+        <div>
+          {review.text}
+          {userId ? (
+            <p><b>By</b>: {user ? user.name : "User not found"}</p>
+          ) : (
+            <p>Author not found</p>
+          )}
+        </div>
+      ) : (
+        <div>Review not found</div>
+      )}
     </>
-  )
-}
+  );
+};
